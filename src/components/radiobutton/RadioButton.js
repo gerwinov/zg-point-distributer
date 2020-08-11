dd2627be3585db2437752b536211bfae7280746a
Gerwin Overeem <gerwin@overeem.io>
2020-08-11 11:59:58 +0200

    Refactor radiobutton to stateless component

--- src/components/radiobutton/RadioButton.js
+++ src/components/radiobutton/RadioButton.js
@@ -4,40 +4,33 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-class RadioButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleInput = this.handleInput.bind(this);
+function RadioButton(props) {
+  function handleInput() {
+    props.onInput(props.value);
   }
 
-  handleInput() {
-    this.props.onInput(this.props.value);
-  }
+  let className = cx({
+    wrapper: true,
+    checked: props.checked,
+  });
 
-  render() {
-    let className = cx({
-      wrapper: true,
-      checked: this.props.checked,
-    });
-
-    return (
-      <div className={className}>
-        <input
-          type="radio"
-          id={`${this.props.name}-${this.props.value}`}
-          name={this.props.name}
-          value={this.props.value}
-          defaultChecked={this.props.checked}
-          className={styles.input}
-          onClick={this.handleInput} />
-        <label htmlFor={`${this.props.name}-${this.props.value}`} className={styles.label}>
-          <img className={styles.icon} src={this.props.icon} alt="peeps_logo" />
-          <h2 className={styles.text}>{ this.props.displayValue }</h2>
-          <span className={styles.circle} />
-        </label>
-      </div>
-    );
-  }
+  return (
+    <div className={className}>
+      <input
+        type="radio"
+        id={`${props.name}-${props.value}`}
+        name={props.name}
+        value={props.value}
+        defaultChecked={props.checked}
+        className={styles.input}
+        onClick={handleInput} />
+      <label htmlFor={`${props.name}-${props.value}`} className={styles.label}>
+        <img className={styles.icon} src={props.icon} alt="peeps_logo" />
+        <h2 className={styles.text}>{ props.displayValue }</h2>
+        <span className={styles.circle} />
+      </label>
+    </div>
+  );
 }
 
 export default RadioButton;

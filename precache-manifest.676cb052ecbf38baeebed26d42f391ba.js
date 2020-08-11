474ccf3b3e0340667b6785ee134df0e5611ce645
Gerwin Overeem <gerwin@overeem.io>
2020-08-11 14:58:46 +0200

    3de1d44

---
+++ precache-manifest.676cb052ecbf38baeebed26d42f391ba.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab56ded71d91118dff4c8f66bfbb6af7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3da506b92fa3dbddea",
+    "url": "/static/css/main.0d9425b9.chunk.css"
+  },
+  {
+    "revision": "21337a2e8fbd1faad416",
+    "url": "/static/js/2.708f6588.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.708f6588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3da506b92fa3dbddea",
+    "url": "/static/js/main.c2aa866d.chunk.js"
+  },
+  {
+    "revision": "834d3ff43f429b20fb05",
+    "url": "/static/js/runtime-main.6a8e0c6f.js"
+  },
+  {
+    "revision": "f436cbb1f1b26d609d4768142e626314",
+    "url": "/static/media/close.f436cbb1.svg"
+  },
+  {
+    "revision": "1c6213faad0f3a8174c52681f55b774a",
+    "url": "/static/media/diodrum-bold.1c6213fa.woff2"
+  },
+  {
+    "revision": "80f62afa07306ed66585afa9aba97f3a",
+    "url": "/static/media/diodrum-bold.80f62afa.woff"
+  },
+  {
+    "revision": "33c0182f10429f4cdc2c92ddb8aab09b",
+    "url": "/static/media/diodrum-medium.33c0182f.woff2"
+  },
+  {
+    "revision": "740a5cc262cea64cd268316dce1a7f9f",
+    "url": "/static/media/diodrum-medium.740a5cc2.woff"
+  },
+  {
+    "revision": "86a9e41094d6d42af2f9524fc256600f",
+    "url": "/static/media/diodrum-regular.86a9e410.woff2"
+  },
+  {
+    "revision": "d656f473b60342f1db2ad77548e3843c",
+    "url": "/static/media/diodrum-regular.d656f473.woff"
+  },
+  {
+    "revision": "10b9bafd9de2ea8907c88b0dae056d2b",
+    "url": "/static/media/diodrum-regularitalic.10b9bafd.woff"
+  },
+  {
+    "revision": "83f6de8dd0310cc1d9bc52d4c8f847d6",
+    "url": "/static/media/diodrum-regularitalic.83f6de8d.woff2"
+  },
+  {
+    "revision": "2f0a72cab76f148cddfb38ff92adcfe0",
+    "url": "/static/media/diodrum-semibold.2f0a72ca.woff"
+  },
+  {
+    "revision": "519e9f998c5ed7fcb1eba2e8f369f7d1",
+    "url": "/static/media/diodrum-semibold.519e9f99.woff2"
+  },
+  {
+    "revision": "72c299d306d80cbbd883ef3403b4f309",
+    "url": "/static/media/diodrum-semibolditalic.72c299d3.woff"
+  },
+  {
+    "revision": "a00fd3ffcd482c50a64c532dc7a0c03b",
+    "url": "/static/media/diodrum-semibolditalic.a00fd3ff.woff2"
+  },
+  {
+    "revision": "3c613d7ca6fe158109cbc140a28166db",
+    "url": "/static/media/downloading.3c613d7c.png"
+  },
+  {
+    "revision": "cf5060fe814a8f18135d31ae80fa53b6",
+    "url": "/static/media/gaming.cf5060fe.png"
+  },
+  {
+    "revision": "6440841eb955e02b09f91722fb17524c",
+    "url": "/static/media/help.6440841e.svg"
+  },
+  {
+    "revision": "d19811213eaebb6e836877bc746691e8",
+    "url": "/static/media/info.d1981121.svg"
+  },
+  {
+    "revision": "f11717b26e5e30c6f132ed9a9d2d7540",
+    "url": "/static/media/people-1.f11717b2.svg"
+  },
+  {
+    "revision": "5656eca9b94ffe2df5ad35f9dd06dec7",
+    "url": "/static/media/people-2.5656eca9.svg"
+  },
+  {
+    "revision": "2aa64ca8fad9a9fd40fabaaf0e412b97",
+    "url": "/static/media/people-3.2aa64ca8.svg"
+  },
+  {
+    "revision": "a98573b9c08db7b213b385e3f90f73b6",
+    "url": "/static/media/people-4.a98573b9.svg"
+  },
+  {
+    "revision": "9dd92134a19d5f7f232afd7fd299db7a",
+    "url": "/static/media/streaming.9dd92134.png"
+  },
+  {
+    "revision": "d4187cc252c1a02dce87b55112e9d617",
+    "url": "/static/media/surfing.d4187cc2.png"
+  },
+  {
+    "revision": "e7cda71d2f11a34c5100495db5cea1ff",
+    "url": "/static/media/videocalling.e7cda71d.png"
+  }
+]);
